be4a032f200a4d5dc7f0ee33e500a650352738dd
Alex N. Jose <alexnj@google.com>
2023-06-21 14:49:57 -0700

    new test case: chromium lcpp

---
+++ testcases/chromium-lcpp-testcases/async-1st-script.js
@@ -0,0 +1,5 @@
+(function(){
+  const img = document.createElement('img');
+  img.src = "https://placehold.co/600x400?text=asynchronously+\n+placed+image+\n+by+1st+level+script";
+  document.body.appendChild(img);
+})();
---
+++ testcases/chromium-lcpp-testcases/sync-script.js
@@ -0,0 +1,5 @@
+(function(){
+  const img = document.createElement('img');
+  img.src = "https://placehold.co/600x400?text=synchronously+\n+placed+image+\n+by+script";
+  document.body.appendChild(img);
+})();
