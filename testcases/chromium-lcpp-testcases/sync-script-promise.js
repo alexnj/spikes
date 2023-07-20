d8f263be1047bed4648eaed76f38d12b35be883b
Alex N. Jose <alexnj@google.com>
2023-07-19 17:17:22 -0700

    Additional tests

---
+++ testcases/chromium-lcpp-testcases/sync-script-promise.js
@@ -0,0 +1,5 @@
+await new Promise(resolve => {
+  const img = document.createElement('img');
+  img.src = "https://placehold.co/600x400?text=synchronously+\n+placed+image+\n+by+script";
+  document.body.appendChild(img);
+});
