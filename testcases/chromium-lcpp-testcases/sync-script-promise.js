78ea9848572f57d64b320229612e9cbaa198efad
Alex N. Jose <alexnj@google.com>
2023-08-07 10:55:35 -0700

    sync script promise

--- testcases/chromium-lcpp-testcases/sync-script-promise.js
+++ testcases/chromium-lcpp-testcases/sync-script-promise.js
@@ -1,5 +1,16 @@
-await new Promise(resolve => {
+function placeLCPImage() {
+  console.log('Placing LCP image');
   const img = document.createElement('img');
   img.src = "https://placehold.co/600x400?text=synchronously+\n+placed+image+\n+by+script";
   document.body.appendChild(img);
+}
+
+(async function() {
+  await new Promise(resolve => {
+    console.log('resolve()');
+    setTimeout(() => {
+      console.log('setTimeout()');
+      placeLCPImage()
+    }, 300);
 });
+})();
